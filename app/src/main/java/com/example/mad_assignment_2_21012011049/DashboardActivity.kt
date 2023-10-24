0663f5e86353fc8217c2b1797b3267333c8272ec
mehabhatt <mehabhatt21@gmail.com>
2023-10-24 19:03:18 +0530

    Assignment-2

--- app/src/main/java/com/example/mad_assignment_2_21012011049/DashboardActivity.kt
+++ app/src/main/java/com/example/mad_assignment_2_21012011049/DashboardActivity.kt
@@ -1,11 +1,39 @@
-package com.example.mad_assignment_2_21012011049
+package com.example.doctorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // Uncomment the following line if you want to display the hamburger icon
+        // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/mad_assignment_2_21012011049/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment_2_21012011049/MainActivity.kt
@@ -1,11 +1,22 @@
-package com.example.mad_assignment_2_21012011049
+package com.example.doctorapp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var getStartedButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton = findViewById(R.id.getstarted)
+
+        getStartedButton.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
     }
 }
